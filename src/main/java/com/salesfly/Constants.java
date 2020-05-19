5623a75e3cf8eda920d9dec48eb46246fe3060fe
salesfly <otto@salesfly.com>
2020-05-20 00:05:41 +0300

    Version 1.0.0

--- src/main/java/com/salesfly/Constants.java
+++ src/main/java/com/salesfly/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     private Constants() {
     }
 
-    public static final String VERSION = "0.9.1";
+    public static final String VERSION = "1.0.0";
 
     public static final String USER_AGENT = "salesfly-java/" + VERSION;
 
